07c46575037cc15cff3e7d6ceab9e98ffb0149f6
Stephen Thirlwall <stephent@strategicdata.com.au>
2017-03-21 15:59:08 +1100

    SD::OpenAPI::Swagger2 validates & expands swagger

--- lib/SD/OpenAPI/Live.pm
+++ lib/SD/OpenAPI/Live.pm
@@ -4,103 +4,33 @@ use warnings;
 
 use Moo;
 
-extends 'SD::OpenAPI';
-with 'SD::OpenAPI::Role::Swagger2';
+use Carp                    qw( croak );
+use Log::Any                qw( $log );
+use SD::OpenAPI::Swagger2   qw( expand_swagger validate_swagger );
+use Try::Tiny;
 
-use Clone                qw( clone );
-use Function::Parameters qw( :strict );
+use Function::Parameters    qw( :strict );
 
-method BUILD {
-    # dies on failure
-    $self->_validate_spec;
+has swagger_path => (
+    is => 'ro',
+    required => 1,
+);
 
-    _expand_references($self->spec, $self->spec->{definitions});
-    _expand_schemas($self->spec->{paths});
-}
+has spec => (
+    is => 'lazy',
+);
 
-fun _expand_references($value, $definition_for) {
-    if (ref $value eq 'HASH') {
-        _expand_hash_references($value, $definition_for);
+method _build_spec {
+    my $swagger = YAML::XS::LoadFile($self->swagger_path);
+
+    try {
+        $swagger = validate_swagger($swagger);
     }
-    elsif (ref $value eq 'ARRAY') {
-        _expand_array_references($value, $definition_for);
-    }
-}
+    catch {
+        croak $self->swagger_path . "$_\n";
+    };
 
-fun _expand_hash_references($hash, $definition_for) {
-    for my $key (keys %$hash) {
-        my $value = $hash->{$key};
-
-        # Recursively expand any hash or array values we find.
-        _expand_references($value, $definition_for);
-
-        if (($key eq '$ref') && ($value =~ m{#/definitions/(.*)})) {
-            if (exists $definition_for->{$1}) {
-                my $replacement = $definition_for->{$1};
-
-                # Remove the '$ref => #/definitions/Something' pair
-                delete $hash->{$key};
-
-                # Insert all the key/value pairs from the replacement hash.
-                @${hash}{ keys %$replacement } = values %$replacement;
-            }
-        }
-    }
-}
-
-fun _expand_array_references($array, $definition_for) {
-    for my $value (@$array) {
-        _expand_references($value, $definition_for);
-    }
-}
-
-fun _expand_schemas($paths) {
-    while (my ($path, $request) = each %$paths) {
-        while (my ($method, $spec) = each %$request) {
-            my @params;
-            for my $param (@{ $spec->{parameters} }) {
-                if (exists $param->{schema}) {
-                    _expand_schema($param);
-                }
-                push(@params, _expand_param($param));
-            }
-            $spec->{parameters} = \@params;
-        }
-    }
-}
-
-fun _expand_param($param) {
-    if ($param->{type} eq 'object') {
-        return _expand_object($param);
-    }
-    if ($param->{type} eq 'array') {
-        return _expand_array($param);
-    }
-    return $param;
-}
-
-fun _expand_object($param) {
-    my %required = map { $_ => 1 } @{ $param->{required} // [] };
-    while (my ($name, $property) = each %{ $param->{properties} }) {
-        $property = _expand_param($property);
-        $property->{name} = $name;
-        $property->{required} = 1 if $required{$name};
-    }
-    return $param;
-}
-
-fun _expand_array($param) {
-    $param->{items} = _expand_object($param->{items});
-    return $param;
-}
-
-fun _expand_schema($param) {
-    my $schema = $param->{schema};
-    for my $key (keys %$schema) {
-        delete $param->{$key}; # XXX: avoid read-only value error :/
-        $param->{$key} = $schema->{$key};
-    }
-    delete $param->{schema};
+    return expand_swagger($swagger);
 }
 
 1;
---
+++ lib/SD/OpenAPI/Swagger2.pm
@@ -0,0 +1,149 @@
+package SD::OpenAPI::Swagger2;
+
+use 5.22.0;
+use warnings;
+
+use Exporter                qw( import );
+use JSON::Validator         qw( );
+
+use Function::Parameters    qw( :strict );
+
+our @EXPORT_OK = qw( expand_swagger validate_swagger );
+
+fun validate_swagger($swagger) {
+    my $validator = JSON::Validator->new;
+    $validator->schema('http://swagger.io/v2/schema.json#');
+    $validator->coerce(booleans => 1);
+
+    if (my @errors = $validator->validate($swagger)) {
+        my $errors = join("\n", map { $_->path . ': ' . $_->message } @errors);
+        die "$errors\n";
+    }
+
+    return $swagger;
+}
+
+fun expand_swagger($swagger) {
+    $swagger = _expand_references($swagger);
+    _merge_required($swagger);
+    _hoist_schemas($swagger);
+    _merge_allofs($swagger);
+
+    return $swagger;
+}
+
+fun _expand_references($swagger) {
+    # I had code to do this, but JSON::Validator can do it for us. Win!
+    my $validator = JSON::Validator->new;
+    $validator->schema($swagger);
+    return $validator->schema->data;
+}
+
+# This function walks the swagger tree, calling $f on each item it finds.
+# The items may be hashes, arrays or scalars. Note that $f gets called
+# after the recursive call, so $f will be applied bottom-up.
+fun _walk_tree($object, $f) {
+    if (ref $object eq 'ARRAY') {
+        _walk_tree($_, $f) for @$object;
+    }
+    elsif (ref $object eq 'HASH') {
+        _walk_tree($_, $f) for values %$object;
+    }
+    $f->($object);
+}
+
+# For schemas and such where we have:
+#   properties => { name => { ... } }
+#   required   => [ list-of-names ]
+# make these the same as regular parameters by setting required=>1 in the
+# properties themselves (just the required ones, there is no required=>0)
+fun _merge_required($root) {
+    _walk_tree($root, fun($object) {
+        return unless ref $object eq 'HASH';
+
+        if (exists $object->{properties} && exists $object->{required}) {
+            $object->{properties}->{$_}->{required} = 1
+                for @{ $object->{required} };
+
+            delete $object->{required};
+        }
+    });
+}
+
+# Schemas with references have already been expanded, so hoist them up one
+# level, as if they were declared inline.
+fun _hoist_schemas($root) {
+    _walk_tree($root, fun($object) {
+        return unless ref $object eq 'HASH';
+
+        if (exists $object->{schema}) {
+            @{$object}{ keys %{ $object->{schema} }}
+                = values %{ $object->{schema} };
+
+            delete $object->{schema};
+        }
+    });
+}
+
+# Merge together any allOf sections. The code looks tricky but the idea is
+# simple.
+# Hash values get merged together so that [ { a => 1 }, { b => 2 } ]
+# becomes { a => 1, b => 2 }
+# List values get concatenated.
+fun _merge_allofs($root) {
+    _walk_tree($root, fun($object) {
+        return unless ref $object eq 'HASH';
+
+        if (exists $object->{allOf}) {
+            for my $sub_object (@{ $object->{allOf} }) {
+                while (my ($key, $value) = each %$sub_object) {
+                    if (ref $value eq 'HASH') {
+                        @{ $object->{$key} }{ keys %$value } = values %$value;
+                    }
+                    elsif (ref $value eq 'ARRAY') {
+                        push(@{ $object->{$key} }, @$value);
+                    }
+                    else {
+                        if (exists $object->{$key} &&
+                            $object->{$key} ne $value) {
+                            die "Merging allof: $object->{$key} ne $value\n";
+                        }
+                        else {
+                            $object->{$key} //= $value;
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+SD::OpenAPI::Swagger2 - validate and expand swagger
+
+=head1 SYNOPSIS
+
+    use SD::OpenAPI::Swagger2 qw( expand_swagger validate_swagger );
+
+    my $swagger_in = ...;
+    validate_swagger($swagger_in);
+    my $swagger_out = expand_swaggger($swagger_in);
+
+=head1 FUNCTIONS
+
+=head2 validate_swagger($swagger)
+
+Validate the provided swagger and die if any errors are found.
+Returns the validated swagger unchanged.
+
+=head2 expand_swagger($swagger)
+
+Expands references, schemas, and allOfs to the corresponding inline versions.
+Returns the expanded swagger.
+
+=cut
