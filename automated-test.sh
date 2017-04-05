bb05e232d4ce5e1b0bc60f2f182fdbde61a520c4
Stephen Thirlwall <stephent@strategicdata.com.au>
2017-04-05 10:02:05 +1000

    Improve automated-test.sh
    Now installs module using cpanm into a base stratperlbase image

--- automated-test.sh
+++ automated-test.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+IMAGE=docker.sdlocal.net/devel/stratperlbase
 WORKDIR=/test
 
-docker-compose build --pull
-docker-compose run --rm sd-openapi minil test --automated
+# This automated test just uses cpanm to attempt to install the module into
+# the stratperlbase image.
+# I originally used the stratperldancer image, but that now has this module
+# installed, which makes the testing a bit less clean room.
+
+docker pull $IMAGE
+docker run --rm -t -v $PWD:$WORKDIR:ro $IMAGE bash -c "
+    # Copy the local dir into temp as we've got in mounted read-only, and
+    # cpanm doesn't like that for reasons unknown.
+    cp -a $WORKDIR /tmp/
+    cd /tmp$WORKDIR
+    echo Installing dependencies
+    cpanm --installdeps -q .
+    echo Installing SD-OpenAPI
+    cpanm -v .
+"
