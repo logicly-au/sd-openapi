40c37785729a6faa62a4fb9463f5e32332810733
Stephen Thirlwall <stephent@strategicdata.com.au>
2017-04-11 15:09:22 +1000

    Refactor type checking into SD::OpenAPI::Types
    Closes #6

--- lib/SD/OpenAPI/Live/Dancer2.pm
+++ lib/SD/OpenAPI/Live/Dancer2.pm
@@ -1,5 +1,6 @@
 package SD::OpenAPI::Live::Dancer2;
 use 5.22.0;
+use warnings;
 
 use Moo;
 extends 'SD::OpenAPI::Live';
@@ -7,15 +8,11 @@ extends 'SD::OpenAPI::Live';
 use Carp                        qw( croak );
 use Clone                       qw( clone );
 use Class::Load                 qw( load_class );
-use DateTime::Format::ISO8601   qw( );
 use Log::Any                    qw( $log );
-use JSON::MaybeXS               qw( is_bool );
+use SD::OpenAPI::Types          qw( check_type prepare_handler );
 use Try::Tiny;
 
-use Function::Parameters qw( :strict );
-
-# This needs to be declared early as it is self-referential.
-my %type_check;
+use Function::Parameters        qw( :strict );
 
 has namespace => (
     is => 'ro',
@@ -182,13 +179,10 @@ method make_handler($metadata) {
         $log->info(" --> $metadata->{module_name}::$metadata->{sub_name}");
     }
 
-    # Install type checkers for all the types.
+    # Install type checkers and defaults for all the types.
     # Do this ahead of time so we only need to check it all once. At run-time
     # we can assume this is all correct.
-    for my $p (@{ $metadata->{parameters} }) {
-        assign_type($p);
-        assign_default($p, $p->{name});
-    }
+    prepare_handler($metadata);
 
     # Wrap the handler $sub in parameter validation/inflation code.
     # The function below is what gets called at run-time.
@@ -227,8 +221,7 @@ method make_handler($metadata) {
             }
 
             try {
-                my $check = $type_check{$p->{check_type}};
-                $params{$name} = $check->($vals[0], $p, $name);
+                $params{$name} = check_type($vals[0], $p, $name);
             }
             catch {
                 @errors{ keys %$_ } = values %$_;
@@ -255,277 +248,6 @@ method make_handler($metadata) {
         }
 
         return $result;
-    }
-}
-
-my $datetime_parser = DateTime::Format::ISO8601->new;
-
-# http://swagger.io/specification/#data-types-12
-# This table contains handlers to check and inflate the incoming types.
-# In assign_type we set a check_type field in each type. This field matches
-# the keys below.
-%type_check = (
-    integer => sub {
-        my ($value, $type, $name) = @_;
-        my $min = $type->{minimum};
-        my $max = $type->{maximum};
-
-        if ($value =~ /^[-+]?\d+$/) {
-            $value = int($value);
-            if ($value >= $min && $value <= $max) {
-                return $value;
-            }
-        }
-        die { $name => "must be an integer in range [$min, $max]" };
-    },
-
-    string => sub {
-        my ($value, $type, $name) = @_;
-        $value = "$value";
-        my $length = length($value);
-        if ($length >= $type->{minLength} && $length <= $type->{maxLength}) {
-            return $value;
-        }
-        die { $name => $type->{msg} };
-    },
-
-    boolean => sub {
-        my ($value, $type, $name) = @_;
-        if (
-            is_bool($value) ||              # decoded body params
-            ($value =~ /^0|false|1|true$/)  # query params are strings
-        ) {
-            return 0 if $value eq 'false';  # special case false, other cases are logical
-            return $value ? 1 : 0; # this works better with postgres
-        }
-        die { $name => "must be a boolean value" };
-    },
-
-    date => sub {
-        my ($value, $type, $name) = @_;
-        try {
-            $value = $datetime_parser->parse_datetime($value);
-        }
-        catch {
-            die { $name => "must be an ISO8601-formatted date string" };
-        };
-        return $value;
-    },
-
-    'date-time' => sub {
-        my ($value, $type, $name) = @_;
-        try {
-            $value = $datetime_parser->parse_datetime($value);
-        }
-        catch {
-            die { $name => "must be an ISO8601-formatted datetime string" };
-        };
-        return $value;
-    },
-
-    range => sub {
-        my ($value, $type, $name) = @_;
-        if (my ($low, $high) = ($value =~ /^(\d+)-(\d+)?$/)) {
-            if ((!defined $high) || ($low <= $high)) {
-                return [ $low, $high ];
-            }
-        }
-        die { $name => "must be a range (eg. 500-599, or 100-)" };
-    },
-
-    sort => sub {
-        my ($value, $type, $name) = @_;
-
-        if ($value =~ $type->{pattern}) {
-            # [ [ '+', 'foo' ], [ '-', 'bar' ] ]
-            # The sign is optional, and defaults to plus. Note that the regex
-            # below deliberately makes the sign non-optional. If we match, we
-            # have an explicit sign, otherwise we have no sign.
-            return [ map { /^([+-])(.*)$/ ? [ $1, $2 ] : [ '+', $_ ] }
-                       split(/,/, $value) ];
-        }
-        die { $name => $type->{error_message} };
-    },
-
-    array => sub {
-        my ($value, $type, $name) = @_;
-        my $itemtype = $type->{items}->{check_type};
-        if (ref $value ne 'ARRAY') {
-            die { $name => "must be a JSON-formatted array of $itemtype" };
-        }
-
-        my $check = $type_check{$itemtype};
-
-        # Collect any errors further down and propagate them up
-        my @ret;
-        my %errors;
-        while (my ($index, $item) = each @$value) {
-            try {
-                push(@ret, $check->($item, $type->{items}, "$name\[$index\]"));
-            }
-            catch {
-                @errors{ keys %$_ } = values %$_;
-            };
-        }
-        die \%errors if keys %errors;
-        return \@ret;
-    },
-
-    object => sub {
-        my ($value, $type, $name) = @_;
-        if (ref $value ne 'HASH') {
-            die { $name => "must be a JSON-formatted object" };
-        }
-
-        my %ret;
-        my %errors;
-        while (my ($field_name, $field_type) = each %{ $type->{properties} }) {
-            my $key = "$name\.$field_name";
-
-            if (!exists $value->{$field_name}) {
-                if (exists $field_type->{default_value}) {
-                    # This is already validated and inflated. Copy it and move
-                    # on to the next parameter. We don't need to fall through.
-                    $ret{$field_name} = $field_type->{default_value};
-                }
-                elsif ($field_type->{required}) {
-                    $errors{$key} = "missing field $field_name";
-                }
-
-                # In all cases we can skip to the next field.
-                next;
-            }
-
-            if (!defined $value->{$field_name}) {
-                if ($field_type->{'x-nullable'}) {
-                    $ret{$field_name} = undef;
-                }
-                else {
-                    $errors{$key} = "null field $field_name";
-                }
-
-                # In all cases we can skip to the next field.
-                next;
-            }
-
-            my $check = $type_check{ $field_type->{check_type} };
-            try {
-                $ret{$field_name} =
-                    $check->($value->{$field_name}, $field_type, $key);
-            }
-            catch {
-                @errors{ keys %$_ } = values %$_;
-            };
-        }
-        die \%errors if keys %errors;
-        return \%ret;
-    },
-);
-
-fun max_value($bits) {
-    return (2 ** ($bits - 1)) - 1;
-}
-
-my %limit = (
-    int32 => { min => -max_value(32) - 1, max => max_value(32) },
-    int64 => { min => -max_value(64) - 1, max => max_value(64) },
-);
-
-# Recursively assign types to the parameters. The swagger params use a two-level
-# hierarchy for the types. We create a single 'check_type' key which maps to
-# the correct handler in the %type_check table.
-fun assign_type($spec) {
-    if ((exists $spec->{format}) && (exists $type_check{ $spec->{format} })) {
-        $spec->{check_type} = $spec->{format};
-    }
-    elsif (exists $type_check{ $spec->{type} }) {
-        $spec->{check_type} = $spec->{type};
-    }
-    else {
-        $log->error("Can't match type for $spec->{name}");
-        #use Data::Dumper::Concise; print STDERR "MISSING: ", Dumper($spec);
-        $spec->{type} = $spec->{check_type} = 'string';
-    }
-
-    if ($spec->{check_type} eq 'integer') {
-        $spec->{format} //= 'int32';
-
-        my $limit = $limit{ $spec->{format} };
-        $spec->{minimum} //= $limit->{min};
-        $spec->{maximum} //= $limit->{max};
-    }
-
-    if ($spec->{check_type} eq 'string') {
-        if (exists $spec->{minLength}) {
-            if (exists $spec->{maxLength}) {
-                $spec->{msg} = "must be a string between $spec->{minLength} and $spec->{maxLength} characters long";
-            }
-            else {
-                $spec->{msg} = "must be a string at least $spec->{minLength} characters long";
-            }
-        }
-        elsif (exists $spec->{maxLength}) {
-            $spec->{msg} = "must be a string of no more than $spec->{maxLength} characters";
-        }
-        else {
-            $spec->{msg} = 'must be a string';
-        }
-
-        $spec->{minLength} //= 0;
-        $spec->{maxLength} //= 2 * 1024 * 1024 * 1024; # that oughta do it...
-    }
-
-    if ($spec->{check_type} eq 'sort') {
-        # Build up the regex that matches the sort spec ahead of time.
-        # If we have an array of x-sort-fields, use those specifically,
-        # otherwise default to \w+.
-        $spec->{error_message} =
-            'must be a comma-separated list of field/+field/-field';
-        my $sign  = qr/[-+]/;
-        my $ident = qr/\w+/;    # default case if no sort fields specified
-        if (my $sort_fields = $spec->{'x-sort-fields'}) {
-            my $pattern = join('|', map { quotemeta } sort @$sort_fields);
-            $ident = qr/(?:$pattern)/;
-
-            $spec->{error_message} .= '. Valid fields are: ' .
-                join(', ', sort @$sort_fields);
-        }
-        my $term = qr/($sign)?($ident)/;
-        $spec->{pattern} = qr/^$term(?:,$term)*$/;
-
-        # TODO: we could replace or augment the description field to list the available sort fields
-    }
-
-    if ($spec->{check_type} eq 'array') {
-        assign_type($spec->{items});
-    }
-    elsif ($spec->{check_type} eq 'object') {
-        assign_type($_) for values %{ $spec->{properties} };
-    }
-}
-
-fun assign_default($spec, $name) {
-    if (exists $spec->{default}) {
-        my $check = $type_check{$spec->{check_type}};
-        try {
-            $spec->{default_value} =
-                $check->($spec->{default}, $spec, $name . '.default');
-        }
-        catch {
-            while (my ($field, $error) = each %$_) {
-                $log->error("$field: $error");
-                #XXX: die here?
-            }
-        };
-    }
-
-    if ($spec->{check_type} eq 'array') {
-        assign_default($spec->{items}, $name);
-    }
-    elsif ($spec->{check_type} eq 'object') {
-        while (my ($key, $value) = each %{ $spec->{properties} }) {
-            assign_default($value, "$name.$key");
-        }
     }
 }
 
